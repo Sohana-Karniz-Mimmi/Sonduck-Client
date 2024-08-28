737d50e97224fc61a1c88a83a967eb8e730379be
DeveloperFahad <developerfahad.me@gmail.com>
2024-08-28 20:45:43 +0600

    Changed website's body bg-color

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import Home from "../Pages/Home";
+import FAQs from "../Pages/FAQs";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
       },
+      {
+        path: "/faqs",
+        element: <FAQs></FAQs>
+      }
     ]
   }
 ]);
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,52 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Navbar = () => {
-    return (
-        <div>
-            this is navbar
+    const navlinks = <>
+    <NavLink>Home</NavLink>
+    <NavLink>Store</NavLink>
+    <NavLink to="/faqs">Faqs</NavLink>
+    </>
+  return (
+    <div>
+      <div className="navbar bg-black">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {navlinks}
+            </ul>
+          </div>
+          <Link to="/" className="uppercase"><span className="text-white">Sonduck</span><span className="text-primary">film</span></Link>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 gap-5">
+            {navlinks}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
