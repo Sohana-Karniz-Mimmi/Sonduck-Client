57954b36b84b5223a048ca51597030b0338ffe68
DeveloperFahad <developerfahad.me@gmail.com>
2024-08-28 18:45:56 +0600

    Create a new branch and added a FAQs page

---
+++ src/Pages/FAQs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from './../Shared/Container';
+
+const FAQs = () => {
+    return (
+        <div>
+            <Container>
+                
+            </Container>
+        </div>
+    );
+};
+
+export default FAQs;
