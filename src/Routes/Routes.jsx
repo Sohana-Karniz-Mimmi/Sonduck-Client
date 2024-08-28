b362b32ee08499287b2d2d96522fb94706567246
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2024-08-28 19:10:45 +0600

    created blog details page and load blog data

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -19,9 +19,15 @@ const router = createBrowserRouter([
         element: <BlogPage />,
       },
       {
-        path: "/blogs/:id",
+        path: "/blogsDetails/:id",
         element: <BlogDetailsPage/>,
         // TODO: give here single products api to fetch data
+        loader: ({ params }) =>
+  fetch(`/blogData.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      return data.find(blog => blog.id == params.id);
+    }),
       }
     ],
   },
