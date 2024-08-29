30d95fee31c72e60f9418d44cbb6fdc01b2f74bc
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-08-29 15:39:57 +0600

    storecard and futurepack create done

---
+++ src/Pages/Store/FuturePack.jsx
@@ -0,0 +1,26 @@
+const FuturePack = () => {
+  return (
+    <div>
+      <div>
+        <img
+          src="
+        "
+          alt=""
+        />
+      </div>
+      <div>
+        <div className="flex mr-5">
+          <h2 className="py-2 px-2">Pr</h2>
+          <h2 className="py-2 px-2">Ae</h2>
+        </div>
+        <div>
+          <h1>Motionduck All Access Pass Subscription</h1>
+          <p>Unlimited Packs Get Every Pack We Have + Every Future Pack!</p>
+          <button>Get Access</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FuturePack;
