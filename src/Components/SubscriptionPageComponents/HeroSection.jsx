636b274180985a40fb640cd9dc96c3f41e60f827
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2024-08-29 12:05:14 +0600

    created subscribe page banner and pricing section

--- src/Components/SubscriptionPageComponents/HeroSection.jsx
+++ src/Components/SubscriptionPageComponents/HeroSection.jsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
           {/* this div is for banner */}
 
           <div className="pt-32 pb-64">
-            <h2 className="title text-center text-4xl lg:text-[64px] font-semibold mb-24 p-2">
+            <h2 className="title text-center text-4xl lg:text-[64px] font-semibold mb-24 p-2 tracking-widest">
               Subscription
             </h2>
             <img src={bannerImg} alt="Subsctiption Image" />
@@ -22,7 +22,7 @@ const HeroSection = () => {
       <div className="px-5">
         <div className="max-w-[1200px]  w-full mx-auto flex flex-col md:flex-row justify-between items-center gap-5 p-5 lg:p-10 border shadow-slate-400 bg-white shadow-lg rounded-2xl md:-mt-60 mb-24">
           <div className="w-full">
-            <h4 className="text-xl font-semibold text-gray-900 text-center mb-5">What is MotionDuck?</h4>
+            <h4 className="text-xl font-semibold text-gray-900 text-center mb-5 tracking-wide">What is MotionDuck?</h4>
             <iframe
               className=" w-full  rounded-2xl"
               height="315"
@@ -35,7 +35,7 @@ const HeroSection = () => {
             ></iframe>
           </div>
           <div className="w-full">
-            <h4 className="text-xl font-semibold text-gray-900 text-center mb-5">Subscription Walkthrough</h4>
+            <h4 className="text-xl font-semibold text-gray-900 text-center mb-5 tracking-wide">Subscription Walkthrough</h4>
             <iframe
               className="w-full rounded-2xl"
               height="315"
--- src/Pages/SubscriptionPage.jsx
+++ src/Pages/SubscriptionPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../Components/SubscriptionPageComponents/HeroSection'
+import Pricing from '../Components/SubscriptionPageComponents/Pricing'
 
 const SubscriptionPage = () => {
   return (
     <div>
       <HeroSection/>
+      <Pricing/>
     </div>
   )
 }
