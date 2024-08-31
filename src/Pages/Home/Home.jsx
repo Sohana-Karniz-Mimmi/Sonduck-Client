e177fd08a27bba2953fb8d15e36cfda559473344
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-09-01 00:43:06 +0600

    get single pack, get bunles, all access pass,unlock sonduck and divcover the reviewer component create and done the work

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,14 @@
+import AccessPassSec from "../../Components/SubscriptionPageComponents/AccessPassSec";
+import Discover from "../../Components/SubscriptionPageComponents/Discover";
+import Features from "../../Components/SubscriptionPageComponents/Features";
+import SinglePackSlider from "../../Components/SubscriptionPageComponents/SinglePackSlider";
 import FuturePack from "../Store/FuturePack";
+import GetBundles from "../Store/GetBundles";
 import Banner from "./Banners/Banner";
 import FeaturedSinglePacks from "./FeaturedSinglePacks";
 import MotionDuck from "./MotionDuck";
 import SinglePack from "./SinglePack";
+import UnlockSonDuck from "./UnlockSonDuck";
 
 const Home = () => {
   return (
@@ -18,9 +24,22 @@ const Home = () => {
       </div>
       <div>
         <FeaturedSinglePacks />
+        {/* <SinglePackSlider /> */}
       </div>
       <div>
         <SinglePack />
+      </div>
+      <div>
+        <GetBundles />
+      </div>
+      <div className="mt-12">
+        <AccessPassSec />
+      </div>
+      <div>
+        <UnlockSonDuck />
+      </div>
+      <div>
+        <Discover />
       </div>
     </div>
   );
